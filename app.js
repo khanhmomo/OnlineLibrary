0d4c751450ae731415819b2544d4d3ab17fd7bc0
Khanh Tran <21280020@student.hcmus.edu.vn>
2021-11-29 06:55:44 +0700

    F

--- app.js
+++ app.js
@@ -12,13 +12,6 @@ const resolvers = require('./resolver/resolver')
 const mongoDataMethods = require('./data/db')
 
 
-if(process.env.NODE_ENV === 'production') {
-	// set static folder
-	app.use(express.static('client/build'));
-	app.get('/', (req, res) => {
-	  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-	});
-  }
 
 // Connect to MongoDB
 const connectDB = async () => {
