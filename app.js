63fca1471a04f2d1d22fe38984c172099e93c2d4
Khanh Tran <21280020@student.hcmus.edu.vn>
2021-11-29 05:37:30 +0700

    F

--- app.js
+++ app.js
@@ -30,6 +30,20 @@ const server = new ApolloServer({
 	context: () => ({ mongoDataMethods })
 })
 
+// ** MIDDLEWARE ** //
+const whitelist = ['http://localhost:4000', 'http://localhost:8080', 'momolibrary.herokuapp.com']
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin)
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable")
+      callback(null, true)
+    } else {
+      console.log("Origin rejected")
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
 
 const app = express()
 app.use(cors())
@@ -37,7 +51,14 @@ server.applyMiddleware({ app })
 
 const PORT = process.env.PORT || 4000
 
-
+if (process.env.NODE_ENV === 'production') {
+	// Serve any static files
+	app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+	app.get('*', function(req, res) {
+	  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+	});
+  }
 
 app.listen(PORT, () =>
 	console.log(`Server ready at http://localhost:4000${server.graphqlPath}`)
