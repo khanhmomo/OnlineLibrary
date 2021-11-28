712369795801684c4d79418586567b8ee1313faa
Khanh Tran <21280020@student.hcmus.edu.vn>
2021-11-29 06:50:34 +0700

    F

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const mongoDataMethods = require('./data/db')
 if(process.env.NODE_ENV === 'production') {
 	// set static folder
 	app.use(express.static('client/build'));
-	app.get('*', (req, res) => {
+	app.get('/', (req, res) => {
 	  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 	});
   }
